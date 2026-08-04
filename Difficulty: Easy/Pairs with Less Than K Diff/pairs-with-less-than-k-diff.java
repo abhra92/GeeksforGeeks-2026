21afd6e22c2c6ef6f5e095f0eea6f965cfca31fc
Abhra <106505722+abhra92@users.noreply.github.com>
2026-08-04 17:52:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with Less Than K Diff/pairs-with-less-than-k-diff.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int countPairs(int arr[], int k) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int count = 0;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            while (arr[right] - arr[left] >= k) {
+                left++;
+            }
+            count += right - left;
+        }
+        return count;
+    }
+}
